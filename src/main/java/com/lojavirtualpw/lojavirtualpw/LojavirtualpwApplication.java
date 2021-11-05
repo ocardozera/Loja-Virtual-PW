8ce820427cc38c82fa0c42d82f4ebb7df071d025
Matheus Cardoso <matheusjosecardoso@outlook.com>
2021-11-05 00:00:41 -0300

    Initial commit

---
+++ src/main/java/com/lojavirtualpw/lojavirtualpw/LojavirtualpwApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtualpw.lojavirtualpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualpwApplication.class, args);
+	}
+
+}
