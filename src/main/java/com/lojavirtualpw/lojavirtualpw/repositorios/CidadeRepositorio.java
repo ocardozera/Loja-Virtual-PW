83676f9c0feed4e7fe13e0c92299e58418c0f5a4
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-02-23 21:44:51 -0300

    Cidade/Estado

---
+++ src/main/java/com/lojavirtualpw/lojavirtualpw/repositorios/CidadeRepositorio.java
@@ -0,0 +1,7 @@
+package com.lojavirtualpw.lojavirtualpw.repositorios;
+
+import com.lojavirtualpw.lojavirtualpw.modelos.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepositorio extends JpaRepository<Cidade, Long> {
+}
